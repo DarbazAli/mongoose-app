5d6fc599c2c572d0df92498ff2be896f93a13ba1
Darbaz Ali <darbaz.a.h@gmail.com>
2020-10-01 09:17:34 +0300

    format code

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
+'use strict'
+console.clear()
+const log = console.log
+
 const express = require('express')
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const app = express();
+const app = express()
 
-app.listen(3000, () => console.log("Listening on 3000"))
-
+app.listen(3000, () => console.log('Listening on 3000'))
 
 // establish a connection to local mongodb database
 // mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
-const mongoUri = "mongodb://localhost:27017/test";
-mongoose.connect(mongoUri, {useUnifiedTopology: true, useNewUrlParser: true});
-
+const mongoUri = 'mongodb://localhost:27017/test'
+mongoose.connect(mongoUri, { useUnifiedTopology: true, useNewUrlParser: true })
 
 // setup template engine
-app.set('views', './views');
-app.set('view engine', 'pug');
-
+app.set('views', './views')
+app.set('view engine', 'pug')
 
 // serve static files
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // create the home url
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Home', message: "Hello There"})
+    res.render('index', { title: 'Home', message: 'Hello There' })
 })
 
 app.get('/about', (req, res) => {
@@ -34,58 +35,66 @@ app.get('/about', (req, res) => {
 
 // seup a rout for person
 app.get('/person', (req, res) => {
-    res.render('person');
+    res.render('person')
 })
-
-
 
 // create a collection with Schema
 const personSchema = new Schema({
     name: String,
     age: Number,
-    nationality: String
+    nationality: String,
 })
 
 // create a model from this schema
 const Person = mongoose.model('Person', personSchema)
 
-
 /*=========================
     CRUD - Create
 =========================*/
-const createAndSavePerson = done => {
-    const jonDoe = new Person({name: "Jon Doe", age: 30, nationality: "US"})
+const createAndSavePerson = (done) => {
+    const jonDoe = new Person({ name: 'Jon Doe', age: 30, nationality: 'US' })
     jonDoe.save((err, data) => {
-        if ( err ) done(err);
+        if (err) done(err)
         done(null, data)
     })
 }
 
 // setup a post route handler to handle post requests to /person
 app.post('/person', (req, res) => {
-    let person = req.body;
+    let person = req.body
 
     // check if all of the filds are provied
-    if ( !person.name || !person.age || !person.nationality ) {
+    if (!person.name || !person.age || !person.nationality) {
         // res.send("Sorry, you provided wrong info")
-        res.render('show_msg', {message: "Sorry, you provided wrong data", type: "error"})
+        res.render('show_msg', {
+            message: 'Sorry, you provided wrong data',
+            type: 'error',
+        })
     } else {
         let newPerson = new Person({
             name: person.name,
             age: person.age,
-            nationality: person.nationality
+            nationality: person.nationality,
         })
 
-        newPerson.save( (err, data) => {
-            if ( err ) res.render('show_msg', {message: 'Database Error', type: 'error'})
-            else res.render('show_msg', {message: 'New person added', person: data})
+        newPerson.save((err, data) => {
+            if (err)
+                res.render('show_msg', {
+                    message: 'Database Error',
+                    type: 'error',
+                })
+            else
+                res.render('show_msg', {
+                    message: 'New person added',
+                    person: data,
+                })
         })
     }
 })
 
 app.get('/people', (req, res) => {
-    Person.find( (err, response) => {
+    Person.find((err, response) => {
         // res.json(response);
-        res.render('people', {people: response})
+        res.render('people', { people: response })
     })
 })
--- query.js
+++ query.js
@@ -2,7 +2,7 @@
     Developing Full stack apps with Node(Express) and MongoDB
 
     Node eco-system is a realtvily new set of programming languages to write full-stack apps in JavaScript.
-    Node: is a javaScritp runtime machine, it allows you to write and run javaScript apps outsie of browser, and you can build webservers with node
+    Node: is a javaScritp runtime machine, it allows you to write and run javaScript apps outside of browser, and you can build webservers with node
 
     Express, is a javaScript Library to develop full-stack apps with node
 
@@ -12,10 +12,9 @@
 */
 
 // We need 2 main packages for this stack
-const mongoose = require('mongoose');  // to enteract with database
-const Schema = mongoose.Schema; // to create Schemas for database documents
-require('dotenv').config(); // to read environment variables from .env
-
+const mongoose = require('mongoose') // to enteract with database
+const Schema = mongoose.Schema // to create Schemas for database documents
+require('dotenv').config() // to read environment variables from .env
 
 /*==========================================
     1) Install and Setup
@@ -23,9 +22,8 @@ require('dotenv').config(); // to read environment variables from .env
         2. define mongodb uri in the .env file
         3. connect to database with .connect()
 ==============================================*/
-const mongo_uri = process.env.MONGO_URI_LOCAL;
-mongoose.connect(mongo_uri, {useNewUrlParser: true, useUnifiedTopology: true});
-
+const mongo_uri = process.env.MONGO_URI_LOCAL
+mongoose.connect(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 /*==========================================
 
@@ -48,8 +46,8 @@ mongoose.connect(mongo_uri, {useNewUrlParser: true, useUnifiedTopology: true});
 const personSchema = new Schema({
     name: String,
     age: Number,
-    nationality: String
-});
+    nationality: String,
+})
 
 // We create a model based on this schema
 const Person = mongoose.model('Person', personSchema)
